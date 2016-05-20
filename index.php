01d1a804bd37783b51a843d258ab9a6d3ae2cd1b
Drew Reed <drew@medhub.com>
2016-05-20 11:44:44 -0400

    Final Test

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<style>
-	html {
-		background:#000000;
-	}
-</style>
 <h1>Hello World</h1>
 
-<p>Step 2. Resync....</p>
-
-<strong style="color:red;">I pushed to production</strong>
-<br/>
-<strong style="color:blue;">I pushed to production second time</strong>
-<br/>
-<br/>
-<br/>
-<strong style="color:yellow;">I pushed to production second time</strong>
-<br/>
-<br/>
-<br/>
-<strong style="color:green;">I hope this is pushed.</strong>
+<p>Updated Text.</p>
